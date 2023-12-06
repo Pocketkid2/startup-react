ab6c4f2c2e827c9587ce04a3927a3f76ffa239bf
Adam Taylor <afranklint16@gmail.com>
2023-12-06 11:02:56 -0700

    Add a basic router to App object and clear out CSS for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Home() {
+	return (
+		<div>
+			<h1>Home Component</h1>
+		</div>
+	);
+}
+
+function Search() {
+	return (
+		<div>
+			<h1>Search Component</h1>
+		</div>
+	);
+}
+
+function Chat() {
+	return (
+		<div>
+			<h1>Chat Component</h1>
+		</div>
+	);
+}
+
+function Profile() {
+	return (
+		<div>
+			<h1>Profile Component</h1>
+		</div>
+	);
+}
+
+function Login() {
+	return (
+		<div>
+			<h1>Login Component</h1>
+		</div>
+	);
+}
+
+function Signup() {
+	return (
+		<div>
+			<h1>Signup Component</h1>
+		</div>
+	);
+}
+
+function App() {
+	const nav_items = [
+		{ name: 'Home', route: '/' },
+		{ name: 'Search', route: '/search' },
+		{ name: 'Chat', route: '/chat' },
+		{ name: 'Profile', route: '/profile' },
+		{ name: 'Login', route: '/login' },
+		{ name: 'Signup', route: '/signup' },
+		{ name: 'Logout', route: '/logout', onClick: handleLogout }
+	];
+
+	function handleLogout(event) {
+		event.preventDefault();
+		console.log('Logout');
+		alert('Logout triggered');
+	}
+
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<nav>
+					{nav_items.map((item, index) => {
+						return (
+							<NavLink
+								key={index}
+								to={item.route}
+								onClick={item.onClick}>
+								{item.name}
+							</NavLink>
+						)
+					})}
+				</nav>
+				<main>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/search" element={<Search />} />
+						<Route path="/chat" element={<Chat />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="*" element={<Navigate to="/" replace />} />
+					</Routes>
+				</main>
+				<footer>
+					Footer
+				</footer>
+			</div>
+		</BrowserRouter>
+	)
 }
 
 export default App
