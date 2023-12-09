5181921215f5318c80bbf3c35ae4c08c824008ef
Adam Taylor <afranklint16@gmail.com>
2023-12-08 18:37:06 -0700

    Create a React component to be used for both the login and signup forms

---
+++ src/SimpleForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function SimpleForm({ form_name, process_submit }) {
+	const [form, setForm] = React.useState({});
+
+	function handleChange(event) {
+		setForm({ ...form, [event.target.name]: event.target.value });
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		process_submit(form);
+	}
+
+	return (
+		<div className="simple-form">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="username">Username</label>
+				<input
+					id="username"
+					name="username"
+					type="text"
+					value={form.username || ''}
+					onChange={handleChange} />
+				<label htmlFor="password">Password</label>
+				<input
+					id="password"
+					name="password"
+					type="password"
+					value={form.password || ''}
+					onChange={handleChange} />
+				<button type="submit">{form_name}</button>
+			</form>
+		</div>
+
+	);
+}
