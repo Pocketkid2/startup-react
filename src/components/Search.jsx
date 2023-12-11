2ed02be3f1e75bba800ea8eb99c1f32d0bff787d
Adam Taylor <afranklint16@gmail.com>
2023-12-11 11:47:45 -0700

    Filters and sorting now work, also added director column

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,6 +8,20 @@ export default function Search() {
 	const [search_results, set_search_results] = React.useState([]);
 	const [search_term, set_search_term] = React.useState('');
 
+	const [runtime_min, set_runtime_min] = React.useState(0);
+	const [runtime_max, set_runtime_max] = React.useState(0);
+
+	const [rating_G, set_rating_G] = React.useState(true);
+	const [rating_PG, set_rating_PG] = React.useState(true);
+	const [rating_PG13, set_rating_PG13] = React.useState(true);
+	const [rating_R, set_rating_R] = React.useState(true);
+
+	const [year_min, set_year_min] = React.useState(0);
+	const [year_max, set_year_max] = React.useState(0);
+
+	const [sort_field, set_sort_field] = React.useState('title');
+	const [sort_order, set_sort_order] = React.useState('asc');
+
 	function load_search_results(id_list) {
 		id_list.map(async (id) => {
 			const response = await fetch(`https://www.omdbapi.com/?apikey=${api_keys.OMDB}&i=${id}`);
@@ -19,7 +33,8 @@ export default function Search() {
 					title: data.Title,
 					year: data.Year,
 					runtime: data.Runtime,
-					rating: data.Rated
+					rating: data.Rated,
+					director: data.Director
 				}]);
 			} else {
 				console.log('Error: ' + response.status, response.statusText);
@@ -53,54 +68,109 @@ export default function Search() {
 		}
 	}
 
-	function handleSearchBarChange(event) {
-		set_search_term(event.target.value);
+	function handleNumericInputChange(event, set_state) {
+		const value = event.target.value;
+		if (!isNaN(value)) {
+			set_state(value);
+		}
 	}
 
 	return (
 		<div className="search-tool">
 			<div className="search-bar">
 				<form onSubmit={handleSubmit}>
-					<input type="text" placeholder="Search" value={search_term} onChange={handleSearchBarChange} />
+					<input type="text" placeholder="Search" value={search_term} onChange={(event) => set_search_term(event.target.value)} />
 					<button type="submit">Search</button>
 				</form>
 			</div>
 			<div className="search-filter">
 				<form>
 					<label>Runtime</label>
-					<input type="text" placeholder="min" />
-					<input type="text" placeholder="max" />
+					<input type="text" placeholder="min" value={runtime_min} onChange={(event) => handleNumericInputChange(event, set_runtime_min)} />
+					<input type="text" placeholder="max" value={runtime_max} onChange={(event) => handleNumericInputChange(event, set_runtime_max)} />
 
 					<label>Rating</label>
-					<input type="checkbox" id="G" name="G" value="G" />
-					<input type="checkbox" id="PG" name="PG" value="PG" />
-					<input type="checkbox" id="PG-13" name="PG-13" value="PG-13" />
-					<input type="checkbox" id="R" name="R" value="R" />
+					<input type="checkbox" id="G" name="G" value="G" checked={rating_G} onChange={() => set_rating_G(!rating_G)} />
+					<input type="checkbox" id="PG" name="PG" value="PG" checked={rating_PG} onChange={() => set_rating_PG(!rating_PG)} />
+					<input type="checkbox" id="PG-13" name="PG-13" value="PG-13" checked={rating_PG13} onChange={() => set_rating_PG13(!rating_PG13)} />
+					<input type="checkbox" id="R" name="R" value="R" checked={rating_R} onChange={() => set_rating_R(!rating_R)} />
 
 					<label>Year</label>
-					<input type="year" placeholder="min" />
-					<input type="year" placeholder="max" />
+					<input type="year" placeholder="min" value={year_min} onChange={(event) => handleNumericInputChange(event, set_year_min)} />
+					<input type="year" placeholder="max" value={year_max} onChange={(event) => handleNumericInputChange(event, set_year_max)} />
 				</form>
 			</div>
 			<div className="search-results">
 				<table>
 					<thead>
 						<tr>
-							<th>Title</th>
-							<th>Year</th>
-							<th>Runtime</th>
-							<th>Rating</th>
+							{
+								[{ field: 'title', label: 'Title' },
+								{ field: 'year', label: 'Year' },
+								{ field: 'runtime', label: 'Runtime' },
+								{ field: 'rating', label: 'Rating' },
+								{ field: 'director', label: 'Director' }].map((column) => {
+									return (
+										<th key={column.field}>
+											<button onClick={() => {
+												if (sort_field === column.field) {
+													if (sort_order === 'asc') {
+														set_sort_order('desc');
+													} else {
+														set_sort_order('asc');
+													}
+												} else {
+													set_sort_field(column.field);
+												}
+											}}>
+												{column.label}
+												{' '}
+												{
+													sort_field === column.field &&
+													(sort_order === 'asc' ? '▲' : '▼')
+												}
+											</button>
+										</th>
+									);
+								})
+							}
 						</tr>
 					</thead>
 					<tbody>
 						{
-							search_results.map((result, index) => {
+							search_results.filter((result) => {
+								const runtime = parseInt(result.runtime.split(' ')[0]);
+								const rating = result.rating;
+
+								const year = parseInt(result.year);
+
+								return (
+									(runtime_min === 0 || runtime >= runtime_min) &&
+									(runtime_max === 0 || runtime <= runtime_max) &&
+									(rating_G || rating_PG || rating_PG13 || rating_R) &&
+									(rating_G && rating === 'G' ||
+										rating_PG && rating === 'PG' ||
+										rating_PG13 && rating === 'PG-13' ||
+										rating_R && rating === 'R') &&
+									(year_min === 0 || year >= year_min) &&
+									(year_max === 0 || year <= year_max)
+								);
+							}).sort((a, b) => {
+								if (sort_order === 'asc') {
+									return b[sort_field].localeCompare(a[sort_field])
+								} else if (sort_order === 'desc') {
+									return a[sort_field].localeCompare(b[sort_field])
+								} else {
+									return 0;
+								}
+							}).map((result, index) => {
 								return (
 									<tr key={index}>
 										<td>{result.title}</td>
 										<td>{result.year}</td>
 										<td>{result.runtime}</td>
 										<td>{result.rating}</td>
+										<td>{result.director}</td>
 									</tr>
 								);
 							})
